6c4be809b7c87108d7869b003f514bc360070e40
fish119 <vvfish119@gmail.com>
2018-01-07 22:40:54 +0800

    Initial commit

---
+++ src/main/java/site/fish119/adminss/AdminssApplication.java
@@ -0,0 +1,12 @@
+package site.fish119.adminss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminssApplication.class, args);
+	}
+}
